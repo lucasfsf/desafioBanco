c3cebc8b1e28b22b07a56835415a34b957d4a7f6
lucasfsf <lucasfreireri@gmail.com>
2024-04-06 15:12:58 -0300

    Finalizado o desafio - Otimizando o Sistema Bancario com Funções Python

--- desafioBanco.py
+++ desafioBanco.py
@@ -1,6 +1,33 @@
+import re
+
+class User:
+    cpfs = []
+
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = self.verify_cpf(cpf) # main_id
+        if not self.cpf:
+            print("Cpf ja cadastrado ou invalido. Verificar.")
+            return
+        self.endereco = endereco
+        self.cpfs.append(self.cpf)
+
+    def verify_cpf(self, cpf):
+        # Define regular expression pattern to match non-digit characters
+        pattern = r'\D'
+        # Remove all non-digit characters and white spaces
+        clean_cpf = re.sub(pattern, '', cpf)
+        if clean_cpf in self.cpfs:
+            return None
+        else:
+            return clean_cpf
+  
 class Conta:
     """Cria conta de banco com funções de extrato, depósito e saque.
     """
+    agencia = "0001"
+    cont_conta = 0
     menu = """
     MENU
     [d] Depositar
@@ -10,11 +37,13 @@ class Conta:
 
     => """
 
-    def __init__(self, saldo = 0):
+    def __init__(self, usuario, saldo = 0):
         self.saldo = saldo
         self.extrato = ""
         self.numero_saques = 0
         self.limite = 500
+        self.usuario = usuario
+        self.numero_conta = self.cont_conta + 1
 
     def operacao(self, opcao):
         if opcao == "d":
@@ -65,10 +94,15 @@ class Conta:
         return nova_conta
     
     def __str__(self):
-        return f"Saldo da conta: {self.saldo}\n"
+        return f"{self.usuario.nome} - Saldo da conta: {self.saldo}\n"
 
+      
 def main():
-    conta = Conta()
+    users = []
+    user = User("João Testador", "01/09/1999", "086.546.325-96", "Rua Teste, 14 - Jaboão - Belo Horizonte/MG")
+    users.append(user)
+    conta = Conta(user)
+    print(conta)
     while True:
         opcao = input(conta.menu)
         if opcao == "q":
